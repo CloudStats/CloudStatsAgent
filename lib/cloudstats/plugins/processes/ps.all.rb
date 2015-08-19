4553319e736084111f6c37da887700b32bb6da9f
Maxim Kostuchenko <kstmaks@gmail.com>
2015-08-19 14:41:36 +0500

    Fetch ps all the time

--- lib/cloudstats/plugins/processes/ps.all.rb
+++ lib/cloudstats/plugins/processes/ps.all.rb
@@ -1,13 +1,13 @@
 CloudStats::Sysinfo.plugin :processes do
-  os :osx do
-    @ps = `ps -eo user,pid,ppid,rss,vsize,pcpu,pmem,command`
-  end
-
-  os :linux do
-    @ps = `ps -eo user,pid,ppid,rss,vsize,pcpu,pmem,command --sort vsize`
-  end
-
   run do
+    os :osx do
+      @ps = `ps -eo user,pid,ppid,rss,vsize,pcpu,pmem,command`
+    end
+
+    os :linux do
+      @ps = `ps -eo user,pid,ppid,rss,vsize,pcpu,pmem,command --sort vsize`
+    end
+
     {
       count: @ps.each_line.count - 1,
       ps: @ps[0..65_535-1], # cut to 64 kb
