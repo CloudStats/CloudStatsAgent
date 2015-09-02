0ea82113df7f714984da3b1b1720401a3f956252
Andrei Gliga <otzy_007@yahoo.com>
2015-09-02 20:08:10 +0300

    download the backup config

--- lib/cloudstats/backup.rb
+++ lib/cloudstats/backup.rb
@@ -1,16 +1,35 @@
 require 'backup'
+require 'net/http'
 
 module CloudStats
   class Backup
     def initialize
-      ::Backup::Config.load(root_path: "#{File.expand_path(File.dirname(__FILE__))}/../../Backup")
+      @config_dir = "#{File.expand_path(File.dirname(__FILE__))}/../../Backup"
+      download_config
+
+      $logger.info "Initializing the backup"
+      ::Backup::Config.load(root_path: @config_dir)
     end
 
     def perform
+      $logger.info "Performing the backup"
       ::Backup::Logger.start!
       ::Backup::Model.all.each do |m|
         ::Backup::Model.find_by_trigger(m.trigger).first.perform!
       end
+    end
+
+    def download_config
+      $logger.info "Downloading backup config..."
+
+      port = Config[:port].nil? ? '' : ":#{Config[:port]}"
+      backup_config_link = "#{Config[:protocol]}://api.#{Config[:domain]}#{port}/agent_api/backups/#{CloudStats.server_key(nil)}?key=#{PublicConfig['key']}"
+
+      open("#{@config_dir}/config.rb", 'w') do |file|
+        file << open(backup_config_link).read
+      end
+
+      $logger.info "Backup config file downoloaded"
     end
   end
 end
