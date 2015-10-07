f085459e9bc19dc467726eaed1b7b928bb838261
Maxim Kostuchenko <kstmaks@gmail.com>
2015-10-07 23:13:21 +0500

    Upgrade version to developer

--- lib/cloudstats/config.rb
+++ lib/cloudstats/config.rb
@@ -20,7 +20,7 @@ Config = {
 
   # agent
   install_path:     ENV['INSTALL_PATH'] || '/home/cloudstats_agent',
-  version:          '1.2.0',
+  version:          '1.2.0.1',
   restart_required: true
 }
 
