94afe2814c6f2548f621ee9760ffefbd101fc990
Maxim Kostuchenko <kstmaks@gmail.com>
2015-10-13 02:01:13 +0500

    Added separate disk support

--- lib/cloudstats/plugins/disk/usage.all.rb
+++ lib/cloudstats/plugins/disk/usage.all.rb
@@ -1,7 +1,8 @@
 CloudStats::Sysinfo.plugin :disk do
 
-  def format(usage)
+  def format(disks, usage)
     {
+      all: disks,
       used: usage[0],
       available: usage[1],
       total: usage[0] + usage[1]
@@ -11,37 +12,38 @@ CloudStats::Sysinfo.plugin :disk do
   os :linux do
     run do
       df = `df -P`.each_line
-      usage = df
+      disks = df
         .map do |line|
           case line
           when /^Filesystem\s+1024-blocks/
             next
           when /^(.+?)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+\%)\s+(.+)$/
-            [$3.to_f * 1024, $4.to_f * 1024]
+            [$1, $3.to_f * 1024, $4.to_f * 1024]
           end
         end
         .reject(&:nil?)
-        .sum2
-      format(usage)
+      usage = disks.map { |x| x[1..2] }.sum2
+      format(disks, usage)
     end
   end
 
   os :osx do
     run do
       df = `df -kl`
-      usage = df
+      disks = df
         .each_line
         .drop(1)
         .map { |l| l.split("\s") }
         .reject { |l| l[0] =~ /localhost/ }
         .map do |line|
           [
+            line[0].to_s,
             line[2].to_f * 1024,
             line[3].to_f * 1024
           ]
         end
-        .sum2
-      format(usage)
+      usage = disks.map { |x| x[1..2] }.sum2
+      format(disks, usage)
     end
   end
 
