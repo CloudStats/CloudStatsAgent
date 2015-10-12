11463da7dfb08603f55551a0ebbafc90b11e5f26
Maxim Kostuchenko <kstmaks@gmail.com>
2015-10-12 17:13:26 +0500

    Upgrade version to live

--- lib/cloudstats/config.rb
+++ lib/cloudstats/config.rb
@@ -20,7 +20,7 @@ Config = {
 
   # agent
   install_path:     ENV['INSTALL_PATH'] || '/home/cloudstats_agent',
-  version:          '1.2.0.5',
+  version:          '1.2.1',
   restart_required: true
 }
 
