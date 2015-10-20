a10759077e69cb0ae3cbfe64cffc601d30ebfca1
Andrei Gliga <otzy_007@yahoo.com>
2015-10-20 14:43:07 +0300

    v bump

--- lib/cloudstats/config.rb
+++ lib/cloudstats/config.rb
@@ -20,7 +20,7 @@ Config = {
 
   # agent
   install_path:     ENV['INSTALL_PATH'] || '/home/cloudstats_agent',
-  version:          '1.2.1.15',
+  version:          '1.2.1.16',
   restart_required: true
 }
 
