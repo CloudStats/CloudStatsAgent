f4517c8d467742ac584d854cc55f31a375ababd6
Andrei Gliga <otzy_007@yahoo.com>
2015-10-28 18:44:45 +0200

    build version bump

--- lib/cloudstats/config.rb
+++ lib/cloudstats/config.rb
@@ -20,7 +20,7 @@ Config = {
 
   # agent
   install_path:     ENV['INSTALL_PATH'] || '/home/cloudstats_agent',
-  version:          '1.3.0.1',
+  version:          '1.3.0.2',
   restart_required: true
 }
 
