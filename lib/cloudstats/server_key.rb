042f18f3fd1923b18ddd7e160cc1be242e357b26
Andrei Gliga <otzy_007@yahoo.com>
2015-11-13 13:15:48 +0200

    server_key_from_file method

--- lib/cloudstats/server_key.rb
+++ lib/cloudstats/server_key.rb
@@ -1,10 +1,6 @@
 module CloudStats
   def self.server_key(info)
-    server_key_path = Config[:server_key_path]
-
-    if File.exists? server_key_path
-      stored_key = File.read(server_key_path).strip
-    end
+    stored_key = CloudStats.server_key_from_file
 
     valid = stored_key && stored_key.length == 32
 
@@ -14,6 +10,14 @@ module CloudStats
       key = self.generate_server_key(info)
       File.write(server_key_path, key)
       key
+    end
+  end
+
+  def self.server_key_from_file
+    server_key_path = Config[:server_key_path]
+
+    if File.exists? server_key_path
+      File.read(server_key_path).strip
     end
   end
 
