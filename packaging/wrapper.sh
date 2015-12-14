bcd47144c67fd5bd5b44c695d110195258c4aef5
Maxim Kostuchenko <kstmaks@gmail.com>
2015-12-14 17:51:57 +0500

    exec keepalive

--- packaging/wrapper.sh
+++ packaging/wrapper.sh
@@ -9,4 +9,4 @@ export BUNDLE_GEMFILE="$SELFDIR/lib/vendor/Gemfile"
 unset BUNDLE_IGNORE_CONFIG
 
 # Run the actual app using the bundled Ruby interpreter.
-"$SELFDIR/keepalive" "$SELFDIR/lib/ruby/bin/ruby" -rbundler/setup "$SELFDIR/lib/app/lib/cloudstats.rb" $@
+exec "$SELFDIR/keepalive" "$SELFDIR/lib/ruby/bin/ruby" -rbundler/setup "$SELFDIR/lib/app/lib/cloudstats.rb" $@
