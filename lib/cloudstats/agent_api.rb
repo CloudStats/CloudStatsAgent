1649d322f684a8793c5da5b1603568537d29a95b
Andrei Gliga <otzy_007@yahoo.com>
2016-07-21 20:33:13 +0300

    statsd_server method

--- lib/cloudstats/agent_api.rb
+++ lib/cloudstats/agent_api.rb
@@ -5,7 +5,7 @@ module CloudStats
     end
 
     def self.grab_server_id
-      uri = URI("#{AgentApi.api_path}/server_id?key=#{PublicConfig['key']}&server_key=#{CloudStats.server_key(nil)}")
+      uri = URI("#{AgentApi.api_path}/server_id?#{AgentApi.params}")
 
       begin
         JSON.parse(Net::HTTP.get(uri))['id']
@@ -16,8 +16,24 @@ module CloudStats
       end
     end
 
+    def self.statsd_server
+      uri = URI("#{AgentApi.api_path}/statsd_server?#{AgentApi.params}")
+
+      begin
+        JSON.parse(Net::HTTP.get(uri))
+      rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Timeout::Error, JSON::ParserError => e
+        $logger.error "Error getting the server id #{e}"
+
+        'udp'
+      end
+    end
+
     def self.api_path
       "#{Config[:protocol]}://api.#{Config[:domain]}:#{Config[:port]}/agent_api"
+    end
+
+    def self.params
+      "key=#{PublicConfig['key']}&server_key=#{CloudStats.server_key(nil)}"
     end
   end
 end
