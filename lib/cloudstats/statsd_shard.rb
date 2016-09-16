ce762a54156eb8bed13dd98a2d1c843145fcbdfa
Andrei Gliga <otzy_007@yahoo.com>
2016-09-16 12:05:10 +0300

    debug print removed

--- lib/cloudstats/statsd_shard.rb
+++ lib/cloudstats/statsd_shard.rb
@@ -3,7 +3,7 @@ module CloudStats
     def self.store_statsd_host
       statsd_host = AgentApi.statsd_shard
       PublicConfig.merge!('statsd_host' => statsd_host)
-      p PublicConfig
+
       PublicConfig.save_to_yml
     end
   end
