c8c14b30705a982e567365827f75d928e93f5778
Andrei Gliga <otzy_007@yahoo.com>
2016-09-22 15:36:16 +0300

    no need to redefine to_json method

--- lib/cloudstats/helpers/hash.rb
+++ lib/cloudstats/helpers/hash.rb
@@ -3,10 +3,6 @@ class ::Hash
     Hash[map { |k, v| yield k, v }]
   end
 
-  def to_json
-    JSON.generate(self)
-  end
-
   def deep_merge!(second)
     merger = proc { |_key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
     self.merge!(second, &merger)
