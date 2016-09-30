80c6f7aa62d2e0f2148cd45c65889e4808434804
Wedge <shadowpilot34@gmail.com>
2016-09-30 17:46:10 +0300

    Lang to C

--- packaging/wrapper.sh
+++ packaging/wrapper.sh
@@ -4,6 +4,8 @@ set -e
 # Figure out where this script is located.
 export SELFDIR="`dirname \"$0\"`"
 export SELFDIR="`cd \"$SELFDIR\" && pwd`"
+export LANG=C
+export LC_ALL=C
 
 export BUNDLE_GEMFILE="$SELFDIR/lib/vendor/Gemfile"
 unset BUNDLE_IGNORE_CONFIG
