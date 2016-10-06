dc0aad2c5cb9ea0183dd79aa673ec0114c5a95b0
Wedge <shadowpilot34@gmail.com>
2016-10-06 18:33:21 +0300

    Make raven skip unwanted exceptions

--- lib/cloudstats/initializers/raven.rb
+++ lib/cloudstats/initializers/raven.rb
@@ -4,5 +4,7 @@ Raven.configure do |config|
   config.dsn = 'https://af2c37d7336b47fa81af5871515b3fc0:619611ea1fde43fd82029f4d326c697c@sentry.io/89500'
 end
 
+config.excluded_exceptions = ['Errno::ETIMEDOUT', 'Errno::ENOMEM', 'Errno::ECONNRESET'] + Raven::Configuration::IGNORE_DEFAULT
+
 Raven.tags_context version: CloudStats::VERSION, agent_environment: CloudStats::ENVIRONMENT
 Raven.extra_context PublicConfig: PublicConfig, Config: Config
