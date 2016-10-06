6b91a199fc52a96e189df24cad526dfd4755ee06
Andrei Gliga <otzy_007@yahoo.com>
2016-10-06 19:11:02 +0300

    a few more error types excluded from raven report

--- lib/cloudstats/initializers/raven.rb
+++ lib/cloudstats/initializers/raven.rb
@@ -2,7 +2,7 @@ require 'raven'
 
 Raven.configure do |config|
   config.dsn = 'https://af2c37d7336b47fa81af5871515b3fc0:619611ea1fde43fd82029f4d326c697c@sentry.io/89500'
-  config.excluded_exceptions = ['Errno::ETIMEDOUT', 'Errno::ENOMEM', 'Errno::ECONNRESET', 'Errno::EPIPE'] + Raven::Configuration::IGNORE_DEFAULT
+  config.excluded_exceptions = ['Errno::ETIMEDOUT', 'Errno::ENOMEM', 'Errno::ECONNRESET', 'Errno::EPIPE', 'Errno::ENOSPC', 'Net::ReadTimeout'] + Raven::Configuration::IGNORE_DEFAULT
 end
 
 Raven.tags_context version: CloudStats::VERSION, agent_environment: CloudStats::ENVIRONMENT
