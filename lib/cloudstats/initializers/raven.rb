c96266ea2b43eb276ddbb68d377d975f920bf2d8
Wedge <shadowpilot34@gmail.com>
2016-10-06 18:34:08 +0300

    Fix previous commit

--- lib/cloudstats/initializers/raven.rb
+++ lib/cloudstats/initializers/raven.rb
@@ -2,9 +2,8 @@ require 'raven'
 
 Raven.configure do |config|
   config.dsn = 'https://af2c37d7336b47fa81af5871515b3fc0:619611ea1fde43fd82029f4d326c697c@sentry.io/89500'
+  config.excluded_exceptions = ['Errno::ETIMEDOUT', 'Errno::ENOMEM', 'Errno::ECONNRESET', 'Errno::EPIPE'] + Raven::Configuration::IGNORE_DEFAULT
 end
-
-config.excluded_exceptions = ['Errno::ETIMEDOUT', 'Errno::ENOMEM', 'Errno::ECONNRESET'] + Raven::Configuration::IGNORE_DEFAULT
 
 Raven.tags_context version: CloudStats::VERSION, agent_environment: CloudStats::ENVIRONMENT
 Raven.extra_context PublicConfig: PublicConfig, Config: Config
