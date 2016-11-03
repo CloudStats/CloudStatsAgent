cf804de05bc61b3cc96d98fb02e4661e6cc06694
Wedge <shadowpilot34@gmail.com>
2016-11-03 14:47:14 +0300

    Updater uses faraday now

--- lib/cloudstats/updater.rb
+++ lib/cloudstats/updater.rb
@@ -1,4 +1,3 @@
-require 'open-uri'
 require_relative 'version.rb'
 
 module CloudStats
@@ -13,6 +12,10 @@ module CloudStats
       @app_dir = Config[:install_path]
       @init_script = PublicConfig['init_script'] || '/etc/init.d/cloudstats-agent'
       @update_type = update_type
+      @conn = Faraday.new do |faraday|
+        faraday.request :url_encoded             # form-encode POST params
+        faraday.adapter Faraday.default_adapter  # make requests with Net::HTTP
+      end
     end
 
     def update
@@ -59,7 +62,7 @@ module CloudStats
     end
 
     def get_latest_version
-      open(@update_server + 'cloudstats-version').read.tr("\n", '')
+      (@conn.get @update_server + 'cloudstats-version').body.tr("\n", '')
     end
 
     def current_version
@@ -70,7 +73,7 @@ module CloudStats
       $logger.info 'Downloading latest version...'
 
       open("/tmp/#{package_name}", 'wb') do |file|
-        file << open(@update_server + package_name).read
+        file << (@conn.get @update_server + package_name).body
       end
       $logger.info 'Donwload completed'
     end
