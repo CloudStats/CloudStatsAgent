2c882821dc9c0ce3e7336fd50c2b4418ea448312
Wedge <shadowpilot34@gmail.com>
2016-11-07 15:02:38 +0300

    Network connections count is read from proc now

--- lib/cloudstats/plugins/network/connections.all.rb
+++ lib/cloudstats/plugins/network/connections.all.rb
@@ -1,5 +1,13 @@
 CloudStats::Sysinfo.plugin :network do
   run do
-    { connections_count: `netstat -an`.each_line.grep(/:/).count }
+    res = {}
+    total = 0
+    ['tcp', 'tcp6', 'udp', 'udp6'].each do |c|
+      num = open("/proc/net/#{c}").readlines.count - 1 rescue 0
+      total += num
+      res["connections_#{c}".to_sym] = num
+    end
+    res[:connections_count] = total
+    res
   end
 end
